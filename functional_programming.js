52f8de01adc8e4d3286ddbd7f0fe70a2c0adb662
Fernando Bahamondes <ferbaco@gmail.com>
2020-08-12 14:12:19 -0400

    Complete functional programming freeCodeCamp exercises

--- functional_programming.js
+++ functional_programming.js
@@ -492,3 +492,257 @@ nonMutatingConcat(first, second);
 
 
 //--Add Elements to the End of an Array Using concat Instead of push
+
+function nonMutatingPush(original, newItem) {
+  
+  let newArr = original.concat(newItem);
+  return newArr;
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+nonMutatingPush(first, second);
+
+//--Use the reduce Method to Analyze Data
+
+// the global variable
+var watchList = [
+  {
+    Title: "Inception",
+    Year: "2010",
+    Rated: "PG-13",
+    Released: "16 Jul 2010",
+    Runtime: "148 min",
+    Genre: "Action, Adventure, Crime",
+    Director: "Christopher Nolan",
+    Writer: "Christopher Nolan",
+    Actors: "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page, Tom Hardy",
+    Plot:
+      "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+    Language: "English, Japanese, French",
+    Country: "USA, UK",
+    Awards: "Won 4 Oscars. Another 143 wins & 198 nominations.",
+    Poster:
+      "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    Metascore: "74",
+    imdbRating: "8.8",
+    imdbVotes: "1,446,708",
+    imdbID: "tt1375666",
+    Type: "movie",
+    Response: "True"
+  },
+  {
+    Title: "Interstellar",
+    Year: "2014",
+    Rated: "PG-13",
+    Released: "07 Nov 2014",
+    Runtime: "169 min",
+    Genre: "Adventure, Drama, Sci-Fi",
+    Director: "Christopher Nolan",
+    Writer: "Jonathan Nolan, Christopher Nolan",
+    Actors: "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+    Plot:
+      "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+    Language: "English",
+    Country: "USA, UK",
+    Awards: "Won 1 Oscar. Another 39 wins & 132 nominations.",
+    Poster:
+      "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+    Metascore: "74",
+    imdbRating: "8.6",
+    imdbVotes: "910,366",
+    imdbID: "tt0816692",
+    Type: "movie",
+    Response: "True"
+  },
+  {
+    Title: "The Dark Knight",
+    Year: "2008",
+    Rated: "PG-13",
+    Released: "18 Jul 2008",
+    Runtime: "152 min",
+    Genre: "Action, Adventure, Crime",
+    Director: "Christopher Nolan",
+    Writer:
+      "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+    Actors: "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+    Plot:
+      "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+    Language: "English, Mandarin",
+    Country: "USA, UK",
+    Awards: "Won 2 Oscars. Another 146 wins & 142 nominations.",
+    Poster:
+      "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+    Metascore: "82",
+    imdbRating: "9.0",
+    imdbVotes: "1,652,832",
+    imdbID: "tt0468569",
+    Type: "movie",
+    Response: "True"
+  },
+  {
+    Title: "Batman Begins",
+    Year: "2005",
+    Rated: "PG-13",
+    Released: "15 Jun 2005",
+    Runtime: "140 min",
+    Genre: "Action, Adventure",
+    Director: "Christopher Nolan",
+    Writer:
+      "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+    Actors: "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+    Plot:
+      "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+    Language: "English, Urdu, Mandarin",
+    Country: "USA, UK",
+    Awards: "Nominated for 1 Oscar. Another 15 wins & 66 nominations.",
+    Poster:
+      "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg",
+    Metascore: "70",
+    imdbRating: "8.3",
+    imdbVotes: "972,584",
+    imdbID: "tt0372784",
+    Type: "movie",
+    Response: "True"
+  },
+  {
+    Title: "Avatar",
+    Year: "2009",
+    Rated: "PG-13",
+    Released: "18 Dec 2009",
+    Runtime: "162 min",
+    Genre: "Action, Adventure, Fantasy",
+    Director: "James Cameron",
+    Writer: "James Cameron",
+    Actors: "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+    Plot:
+      "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+    Language: "English, Spanish",
+    Country: "USA, UK",
+    Awards: "Won 3 Oscars. Another 80 wins & 121 nominations.",
+    Poster:
+      "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+    Metascore: "83",
+    imdbRating: "7.9",
+    imdbVotes: "876,575",
+    imdbID: "tt0499549",
+    Type: "movie",
+    Response: "True"
+  }
+];
+
+function getRating(watchList){
+
+  var averageRating =
+  watchList
+    // Use filter to find films directed by Christopher Nolan
+    .filter(film => film.Director === "Christopher Nolan")
+    // Use map to convert their ratings from strings to numbers
+    .map(film => Number(film.imdbRating))
+    // Use reduce to add together their ratings
+    .reduce((sumOfRatings, rating) => sumOfRatings + rating) /
+  // Divide by the number of Nolan films to get the average rating
+  watchList.filter(film => film.Director === "Christopher Nolan").length;
+
+  return averageRating;
+}
+console.log(getRating(watchList));
+
+
+//-- Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem
+
+const squareList = (arr) => {
+ 
+  let posNum = arr.filter(num => num > 0 && Number.isInteger(num));
+  let squareNums = posNum.map(num => num**2);
+
+  return squareNums;
+};
+
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+
+console.log(squaredIntegers);
+
+
+//-- Sort an Array Alphabetically using the sort Method
+
+function alphabeticalOrder(arr) {
+
+  let sortedArr = arr.sort(function(a,b)
+  {
+    return a === b ? 0 : a > b ? 1 : -1;
+  });
+
+  return sortedArr;
+
+}
+console.log(alphabeticalOrder(["a", "d", "c", "a", "z", "g"]));
+
+//-- Return a Sorted Array Without Changing the Original Array
+
+var globalArray = [5, 6, 3, 2, 9];
+function nonMutatingSort(arr) {
+  let sortedArray = [].concat(arr).sort(function(a,b){return a-b});
+  return sortedArray;
+
+}
+nonMutatingSort(globalArray);
+
+//-- Split a String into an Array Using the split Method
+
+function splitify(str) {
+  
+  return str.split(/\W/);
+
+}
+console.log(splitify("Hello World,I-am code"));
+
+// -- Combine an Array into a String Using the join Method
+
+function sentensify(str) {
+
+  let text = str.split(/\W/);
+  let newStr = text.join(" ");
+  return newStr
+}
+sentensify("May-the-force-be-with-you");
+
+
+// -- Apply Functional Programming to Convert Strings to URL Slugs
+
+function urlSlug(title) {
+  let strTitle  = title.split(/\W/);
+  strTitle = strTitle.filter(function(str) {
+    return /\S/.test(str);
+});
+  let newTitle = strTitle.join("-").toLowerCase();
+  return newTitle;
+}
+
+// -- Use the every Method to Check that Every Element in an Array Meets a Criteria
+
+function checkPositive(arr) {
+
+  return arr.every(function(value){return value > 0});
+}
+checkPositive([1, 2, 3, -4, 5]);
+
+// -- Use the some Method to Check that Any Elements in an Array Meet a Criteria
+
+function checkPositive(arr) {
+  
+  return arr.some(function(value){return value > 0});
+}
+checkPositive([1, 2, 3, -4, 5]);
+
+
+// -- Introduction to Currying and Partial Application
+
+function add(x) {
+  
+  const curriedFunc = y => z => x + y + z;
+  return curriedFunc;
+}
+console.log(add(10)(20)(30));
+
+
+
